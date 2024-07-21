052967cf7d479e1adde828c99065c96c4ece979d
Matheus Della <matheusdellacrocce@hotmail.com>
2024-07-21 20:54:34 -0300

    remove .0

--- scritps/formatNumber.js
+++ scritps/formatNumber.js
@@ -1,11 +1,16 @@
 export function formatNumber(num) {
+    let formattedNumber;
     if (num >= 1e9) {
-        return (num / 1e9).toFixed(3) + 'B';
+        formattedNumber = (num / 1e9).toFixed(3) + 'B';
     } else if (num >= 1e6) {
-        return (num / 1e6).toFixed(2) + 'M';
+        formattedNumber = (num / 1e6).toFixed(2) + 'M';
     } else if (num >= 1e3) {
-        return (num / 1e3).toFixed(1) + 'K';
+        formattedNumber = (num / 1e3).toFixed(1) + 'K';
     } else {
         return num.toString();
     }
+
+    formattedNumber = formattedNumber.replace(/\.0+([BMK])$/, '$1');
+
+    return formattedNumber;
 }
